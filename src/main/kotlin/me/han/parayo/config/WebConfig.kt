b90ab2ff73eef74dae739cd956d711ceb1f7ba44
hanseonghye <hans12312@naver.com>
2021-01-20 23:30:28 +0900

    210120

---
+++ src/main/kotlin/me/han/parayo/config/WebConfig.kt
@@ -0,0 +1,18 @@
+package me.han.parayo.config
+
+import me.han.parayo.interceptor.TokenValidationInterceptor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Configurable
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig @Autowired constructor(
+        private val tokenValidationInterceptor: TokenValidationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(tokenValidationInterceptor)
+                .addPathPatterns("/api/**")
+    }
+}
---
+++ src/main/kotlin/me/han/parayo/domain/product/registration/ProductImageUploadResponse.kt
@@ -0,0 +1,6 @@
+package me.han.parayo.domain.product.registration
+
+data class ProductImageUploadResponse(
+        val productImageId: Long,
+        val filePath: String
+)
